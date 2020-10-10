efc2e72c9296939be5145078308e5feb1e3af650
Serhiy-pro <71960410+Serhiy-pro@users.noreply.github.com>
2020-10-10 13:14:33 +0300

    Home work 2

---
+++ homework/homework_2.php
@@ -0,0 +1,42 @@
+<?php
+$task_manager = [
+    [
+        'ID' => '1',
+        'title' => 'Homework_1',
+        'description' => 'Maths',
+        'responsible' => 'Stan Marsh',
+        'dedline' => 'Tuesday',
+        'status' => 'Done',
+        'subtask' => [
+            'sub_ID' => '1.1',
+            'sub_title' => 'Homework_1.1',
+        ],
+    ],
+    [
+        'ID' => '2',
+        'title' => 'Homework_2',
+        'description' => 'Chemistry',
+        'responsible' => 'Kenny McCormick',
+        'dedline' => 'Friday',
+        'status' => 'postponed',
+        'subtask' => [
+            'sub_ID' => '2.1',
+            'sub_title' => 'Homework_2.2',
+        ],
+    ],
+    [
+        'ID' => '3',
+        'title' => 'Homework_3',
+        'description' => 'Physics',
+        'responsible' => 'Eric Cartman',
+        'dedline' => 'Monday',
+        'status' => 'Not done',
+        'subtask' => [
+            'sub_ID' => '3.1',
+            'sub_title' => 'Homework_3.3',
+        ],
+    ],
+];
+
+var_dump($task_manager);
+
